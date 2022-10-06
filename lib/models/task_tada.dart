bbbcd4b0d61ad8f3ca15d1f8a04a539a1d2e33f9
norah Mohammed <norahmohammed@norahs-MacBook-Pro.local>
2022-10-06 13:52:34 +0300

    test

--- lib/models/task_tada.dart
+++ lib/models/task_tada.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:to_do_list/models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [Task(name: "workout"), Task(name: "got to ....")];
+  List<Task> tasks = [Task(name: "workout at "), Task(name: "got to ....")];
   void addTask(String newTaskTiltle) {
     tasks.add(Task(name: newTaskTiltle));
     notifyListeners();
   }
 
   void updateTask(Task task) {
-    task .doneChange();
-    notifyListeners(); 
-  } 
-   void deleteTask(Task task) {
+    task.doneChange();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
     tasks.remove(task);
-    notifyListeners(); 
+    notifyListeners();
   }
 }
